06f75b6ac42984b7e09ee9e37f5a92c579c0cc9d
GorrettyOkumbe <okumbegorretty4@gmail.com>
2023-01-13 01:02:26 +0300

    adding a new column to a table

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- creating a table with 3 columns
+-- setting id as Primary key and email as unique
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country ENUM('US', 'CO', 'TN') NOT NULL,
+	PRIMARY KEY (id)
+	);
